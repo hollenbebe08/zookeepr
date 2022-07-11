80dad35f068a10ad6a85b53de89d68315eccd40b
Betsy <betsy.griffith12@gmail.com>
2022-07-11 14:04:57 -0400

    added html load for main page and animal page

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3001;
 //instantiate(create) the server
 const app = express();
 
+//middleware that instructs the server to make certain files readily available (such as css and js)
+app.use(express.static('public'));
+
 //parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
 //parse incoming JSON data
@@ -128,6 +131,28 @@ app.post('/api/animals', (req, res) => {
 
     res.json(req.body);
     }
+});
+
+//route to get index.html served from the server
+//NOte the '/' brings us to the root route of the server & is the route used to create a homepage for a server.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//route will take us to the animals page (note api is not included)
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+//route will take us to the zookeepers page (note api is not included)
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//wildcard route to catch non-existent pages/routes
+//NOTE: the wildcard route should ALWAYS come last
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //method to make the server listen
